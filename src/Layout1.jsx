5d3c57a82c674430b1f9153f99324c0a9fccf369
anurag-chandra9 <anuragkrstm01@gmail.com>
2024-09-24 12:55:07 +0530

    first commit

--- src/Layout1.jsx
+++ src/Layout1.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/header/Header.jsx'
-import Footer from './components/footer/footer'
+import Footer from './components/footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
 export default function Layout() {
   return (
